4cc963c45c49f4b83db300d99a37a30428179de0
Jeremy Meng <yumeng@microsoft.com>
2016-04-05 11:57:38 -0700

    Add a FluentValidation example for .NET Core.

---
+++ HelloMvc/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloMvc.Models;
+using HelloMvc.Validators;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [HttpGet("/Customer/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("/Customer/")]
+        public ActionResult Index(Customer model)
+        {
+            CustomerValidator validator = new CustomerValidator();
+            var result = validator.Validate(model);
+            if (result.IsValid)
+            {
+                ViewBag.Name = "Valid: " + model.Name;
+                ViewBag.Email = "Valid: " + model.Email;
+            }
+            else
+            {
+                foreach (var failer in result.Errors)
+                {
+                    ModelState.AddModelError(failer.PropertyName, failer.ErrorMessage);
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ HelloMvc/Validators/ProductValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using HelloMvc.Models;
+
+namespace HelloMvc.Validators
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email is not valid");
+        }
+    }
+}
